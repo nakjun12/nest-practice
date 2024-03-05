1a1a2fa8736e32994417cbc6fd5f018eaf6e4c3d
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-03-06 00:09:59 +0900

    nomard-coders nestJS로 API 만들기 2.2 완
    2.2 Movies Service part One

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Movie } from './entities/Movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  //   @Get('search')
+  //   search(@Query('year') searchingYear: string) {
+  //     return `We are searching for a movie made after: ${searchingYear}`;
+  //   }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
